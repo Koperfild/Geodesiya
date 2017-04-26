55f746417ba27234dc23e79d33c1987178c85c62
Koperfild <Avalancher2@rambler.ru>
2017-04-26 13:18:40 +0300

    лала

--- Геодезия/Геодезия/Form1.Designer.cs
+++ Геодезия/Геодезия/Form1.Designer.cs
@@ -28,12 +28,167 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.ChooseFileBtn = new System.Windows.Forms.Button();
+            this.XTtextBox = new System.Windows.Forms.TextBox();
+            this.YTextBox = new System.Windows.Forms.TextBox();
+            this.SecondsTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.AddPointBtn = new System.Windows.Forms.Button();
+            this.WritePointsToFileBtn = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // ChooseFileBtn
+            // 
+            this.ChooseFileBtn.Location = new System.Drawing.Point(221, 206);
+            this.ChooseFileBtn.Name = "ChooseFileBtn";
+            this.ChooseFileBtn.Size = new System.Drawing.Size(122, 108);
+            this.ChooseFileBtn.TabIndex = 0;
+            this.ChooseFileBtn.Text = "Загрузить файл с измерениями для дальнейших расчётов";
+            this.ChooseFileBtn.UseVisualStyleBackColor = true;
+            this.ChooseFileBtn.Click += new System.EventHandler(this.ChooseFileBtn_Click);
+            // 
+            // XTtextBox
+            // 
+            this.XTtextBox.Location = new System.Drawing.Point(67, 76);
+            this.XTtextBox.Name = "XTtextBox";
+            this.XTtextBox.Size = new System.Drawing.Size(100, 22);
+            this.XTtextBox.TabIndex = 1;
+            // 
+            // YTextBox
+            // 
+            this.YTextBox.Location = new System.Drawing.Point(187, 76);
+            this.YTextBox.Name = "YTextBox";
+            this.YTextBox.Size = new System.Drawing.Size(100, 22);
+            this.YTextBox.TabIndex = 2;
+            // 
+            // SecondsTextBox
+            // 
+            this.SecondsTextBox.Location = new System.Drawing.Point(306, 76);
+            this.SecondsTextBox.Name = "SecondsTextBox";
+            this.SecondsTextBox.Size = new System.Drawing.Size(100, 22);
+            this.SecondsTextBox.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(67, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "X";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(184, 39);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(17, 17);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Y";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(303, 39);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Seconds";
+            // 
+            // AddPointBtn
+            // 
+            this.AddPointBtn.Location = new System.Drawing.Point(306, 113);
+            this.AddPointBtn.Name = "AddPointBtn";
+            this.AddPointBtn.Size = new System.Drawing.Size(104, 55);
+            this.AddPointBtn.TabIndex = 7;
+            this.AddPointBtn.Text = "Добавить точку";
+            this.AddPointBtn.UseVisualStyleBackColor = true;
+            this.AddPointBtn.Click += new System.EventHandler(this.AddPointBtn_Click);
+            // 
+            // WritePointsToFileBtn
+            // 
+            this.WritePointsToFileBtn.Enabled = false;
+            this.WritePointsToFileBtn.Location = new System.Drawing.Point(67, 206);
+            this.WritePointsToFileBtn.Name = "WritePointsToFileBtn";
+            this.WritePointsToFileBtn.Size = new System.Drawing.Size(120, 108);
+            this.WritePointsToFileBtn.TabIndex = 8;
+            this.WritePointsToFileBtn.Text = "Сохранить измерения в файл";
+            this.WritePointsToFileBtn.UseVisualStyleBackColor = true;
+            this.WritePointsToFileBtn.Click += new System.EventHandler(this.WritePointsToFileBtn_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(435, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 17);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Points added:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(498, 170);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 62);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Найти скважину";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(495, 277);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(46, 18);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "label5";
+            this.label5.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(694, 350);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.WritePointsToFileBtn);
+            this.Controls.Add(this.AddPointBtn);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.SecondsTextBox);
+            this.Controls.Add(this.YTextBox);
+            this.Controls.Add(this.XTtextBox);
+            this.Controls.Add(this.ChooseFileBtn);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button ChooseFileBtn;
+        private System.Windows.Forms.TextBox XTtextBox;
+        private System.Windows.Forms.TextBox YTextBox;
+        private System.Windows.Forms.TextBox SecondsTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button AddPointBtn;
+        private System.Windows.Forms.Button WritePointsToFileBtn;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
     }
 }
 
--- Геодезия/Геодезия/Form1.cs
+++ Геодезия/Геодезия/Form1.cs
@@ -3,18 +3,151 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Serialization;
 
 namespace Геодезия
 {
     public partial class Form1 : Form
     {
+        Measurements inputedMeasures = new Measurements();
+        Measurements loadedMeasures = new Measurements();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void ChooseFileBtn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.Filter = "txt files (*.txt, *.doc, *.docx)|*.txt; *.doc; *.docx;";
+            //openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+            string str;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    XmlSerializer ser = new XmlSerializer(typeof(Measurements));
+
+                    XmlReader reader = XmlReader.Create(openFileDialog1.OpenFile());
+                    this.loadedMeasures = (Measurements)ser.Deserialize(reader);
+                    MessageBox.Show("Измерения успешно загружены из файла");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Не удалось загрузить данные из файла");
+                }
+            }
+        }
+
+        private void AddPointBtn_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(XTtextBox.Text) || String.IsNullOrEmpty(YTextBox.Text) || string.IsNullOrEmpty(SecondsTextBox.Text))
+            {
+                ShowError();
+                return;
+            }
+            try
+            {
+                double x = double.Parse(XTtextBox.Text);
+                double y = double.Parse(YTextBox.Text);
+                double seconds = double.Parse(SecondsTextBox.Text);
+                inputedMeasures.points.Add(new EmitPoint { X = x, Y = y, Seconds = seconds });
+                label2.Text = "Points added: " + inputedMeasures.points.Count;
+                WritePointsToFileBtn.Enabled = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Неверно введены измерения");
+            }
+
+
+
+
+        }
+        async private void ShowError()
+        {
+            //Task<string> t = new Task<string>.Run();
+            Task<bool> t = Task.Run(() =>
+            {
+                Thread.Sleep(1500);
+                return false;
+            });
+            Label lbl = new Label { Text = "Не заполнены все поля" };
+            lbl.AutoSize = true;
+            lbl.BringToFront();
+            lbl.Visible = true;
+            lbl.Location = new Point(XTtextBox.Location.X, 113);
+            lbl.ForeColor = Color.Red;
+            lbl.Font = new Font("Microsoft Sans Serif", 10, FontStyle.Bold);
+            this.Controls.Add(lbl);
+            lbl.Visible = await t;
+        }
+
+        private void WritePointsToFileBtn_Click(object sender, EventArgs e)
+        {
+            XmlSerializer ser = new XmlSerializer(typeof(Measurements));
+
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                using (StreamWriter writer = new StreamWriter(saveFileDialog1.OpenFile()))
+                {
+                    ser.Serialize(writer, this.inputedMeasures);
+                }
+                MessageBox.Show("Данные были успешно записаны в файл");
+            }
+        }
+        public class Measurements
+        {
+            public List<EmitPoint> points = new List<EmitPoint>();
+        }
+        public class EmitPoint
+        {
+            public double X { get; set; }
+            public double Y { get; set; }
+            public double Seconds { get; set; }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (this.loadedMeasures == null)
+            {
+                MessageBox.Show("Не загружены данные из файла");
+                return;
+            }
+            if (this.loadedMeasures.points.Count == 1)
+            {
+                MessageBox.Show("1-ого измерения недостаточно для расчётов");
+                return;
+            }
+            var deltaX = Math.Sqrt(Math.Pow(this.loadedMeasures.points[0].X - this.loadedMeasures.points[1].X, 2) + Math.Pow(this.loadedMeasures.points[0].Y - this.loadedMeasures.points[1].Y, 2));
+            var r1 = DistanceFromSignalTime(3000000D,this.loadedMeasures.points[0].Seconds);
+            var r2 = DistanceFromSignalTime(3000000D, this.loadedMeasures.points[1].Seconds);
+
+            var angleAlpha = Math.Acos((Math.Pow(deltaX, 2) + Math.Pow(r1, 2) - Math.Pow(r2, 2)) / (2 * deltaX * r1));
+            //Вектор направления от т. А до т. C
+            var Vx = Math.Cos(angleAlpha);
+            var Vy = -Math.Sin(angleAlpha);
+            
+            var Cx = r1 * Vx + this.loadedMeasures.points[0].X;
+            var Cy = r1 * Vy + this.loadedMeasures.points[0].Y;
+            label5.Visible = true;
+            label5.Text = "Координаты скважины:\nX: " + Cx + "\nY:" + Cy;
+        }
+        private double DistanceFromSignalTime(double lightSpeed, double seconds)
+        {
+            return lightSpeed*seconds/2;
         }
     }
 }
